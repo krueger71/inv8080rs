df2f17220e52fe4d0f1bab40a1e8516a27ca3c2b
Fredrik Andersson <krueger71@users.noreply.github.com>
2024-12-07 15:24:01 +0100

    Refactor emulation loop

--- src/emu/mod.rs
+++ src/emu/mod.rs
@@ -33,65 +33,60 @@ pub struct Options {
 pub struct Emu {
     /// CPU-model
     cpu: Cpu,
-    /// Scale of the display
-    scale: u8,
-    /// Foreground color of display
-    color: u32,
-    /// Background color of display
-    background: u32,
+    /// Options
+    options: Options,
     /// Display frames per second
     fps: u32,
     /// Frequency of CPU, number of cycles per second
     freq: u32,
+    /// Emulator should quit
+    quit: bool,
 }
 
 impl Emu {
     pub fn new(cpu: Cpu, options: Options) -> Self {
         Emu {
             cpu,
-            scale: options.scale,
-            color: options.color,
-            background: options.background,
+            options,
             fps: FPS,
             freq: FREQ,
+            quit: false,
         }
     }
 
     pub fn run(&mut self) {
-        let sdl = sdl2::init().unwrap();
-        let video = sdl.video().unwrap();
-
-        let window = video
+        let sdl2 = sdl2::init().expect("Could not initialize SDL");
+        let mut canvas =
+            sdl2
+            .video()
+            .expect("Could not initialize video")
             .window(
                 "Intel 8080 Space Invaders Emulator",
-                DISPLAY_WIDTH * self.scale as u32,
-                DISPLAY_HEIGHT * self.scale as u32,
+                DISPLAY_WIDTH * self.options.scale as u32,
+                DISPLAY_HEIGHT * self.options.scale as u32,
             )
             .position_centered()
             .build()
-            .unwrap();
-        let mut canvas = window
+            .expect("Could not open window")
             .into_canvas()
-            .present_vsync()
-            .accelerated()
             .build()
-            .unwrap();
+            .expect("Could not create canvas");
 
         // Support alpha blending
         canvas.set_blend_mode(BlendMode::Blend);
 
         let background_color = Color::RGBA(
-            ((self.background & 0xff0000) >> 16) as u8,
-            ((self.background & 0x00ff00) >> 8) as u8,
-            (self.background & 0x0000ff) as u8,
-            ((self.background & 0xff000000) >> 24) as u8,
+            ((self.options.background & 0xff0000) >> 16) as u8,
+            ((self.options.background & 0x00ff00) >> 8) as u8,
+            (self.options.background & 0x0000ff) as u8,
+            ((self.options.background & 0xff000000) >> 24) as u8,
         );
 
         let foreground_color = Color::RGBA(
-            ((self.color & 0xff0000) >> 16) as u8,
-            ((self.color & 0x00ff00) >> 8) as u8,
-            (self.color & 0x0000ff) as u8,
-            ((self.color & 0xff000000) >> 24) as u8,
+            ((self.options.color & 0xff0000) >> 16) as u8,
+            ((self.options.color & 0x00ff00) >> 8) as u8,
+            (self.options.color & 0x0000ff) as u8,
+            ((self.options.color & 0xff000000) >> 24) as u8,
         );
 
         // Create an overlay grid for pixelation effect as a texture
@@ -99,10 +94,10 @@ impl Emu {
         let mut grid = texture_creator
             .create_texture_target(
                 PixelFormatEnum::ARGB8888,
-                DISPLAY_WIDTH * self.scale as u32,
-                DISPLAY_HEIGHT * self.scale as u32,
+                DISPLAY_WIDTH * self.options.scale as u32,
+                DISPLAY_HEIGHT * self.options.scale as u32,
             )
-            .unwrap();
+            .expect("Could not create texture");
         grid.set_blend_mode(BlendMode::Blend);
 
         canvas
@@ -111,32 +106,35 @@ impl Emu {
                 grid_color.a = 0x48;
                 c.set_draw_color(grid_color);
                 // Draw horizontal lines
-                for y in 0..(DISPLAY_HEIGHT * self.scale as u32) {
-                    if y % (self.scale as u32) == 0 {
+                for y in 0..(DISPLAY_HEIGHT * self.options.scale as u32) {
+                    if y % (self.options.scale as u32) == 0 {
                         c.draw_line(
                             (0, y as i32),
-                            ((self.scale as u32 * DISPLAY_WIDTH) as i32, y as i32),
+                            ((self.options.scale as u32 * DISPLAY_WIDTH) as i32, y as i32),
                         )
-                        .unwrap();
+                        .expect("Could not draw horizontal lines on texture");
                     }
                 }
                 // Draw vertical lines
-                for x in 0..(DISPLAY_WIDTH * self.scale as u32) {
-                    if x % (self.scale as u32) == 0 {
+                for x in 0..(DISPLAY_WIDTH * self.options.scale as u32) {
+                    if x % (self.options.scale as u32) == 0 {
                         c.draw_line(
                             (x as i32, 0),
-                            (x as i32, (self.scale as u32 * DISPLAY_HEIGHT) as i32),
+                            (
+                                x as i32,
+                                (self.options.scale as u32 * DISPLAY_HEIGHT) as i32,
+                            ),
                         )
-                        .unwrap();
+                        .expect("Could not draw vertical lines on texture");
                     }
                 }
             })
-            .unwrap();
+            .expect("Could not draw on texture");
 
         // The logical size is set to the size of the display. It makes it possible to draw single pixels at the correct position and get a scaled display automatically
         canvas
             .set_logical_size(DISPLAY_WIDTH, DISPLAY_HEIGHT)
-            .unwrap();
+            .expect("Could not set a logical size for canvas");
 
         println!(
             "{:?}, default_pixel_format: {:?}, scale: {:?}, logical_size: {:?}, output_size: {:?}, render_target_supported: {:?}",
@@ -144,11 +142,11 @@ impl Emu {
             canvas.default_pixel_format(),
             canvas.scale(),
             canvas.logical_size(),
-            canvas.output_size().unwrap(),
+            canvas.output_size().expect("Could not get output size from canvas"),
             canvas.render_target_supported()
         );
 
-        let audio_subsystem = sdl.audio().unwrap();
+        let audio_subsystem = sdl2.audio().expect("Could not initialize audio");
 
         println!(
             "{} {:?}",
@@ -156,54 +154,17 @@ impl Emu {
             audio_subsystem
         );
 
-        let mut events = sdl.event_pump().unwrap();
+        let mut events = sdl2.event_pump().expect("Could not get event pump");
         let cycles_per_frame = self.freq / self.fps;
 
-        'main: loop {
+        while !self.quit {
             let t = Instant::now();
 
             // Handle input
-            for event in events.poll_iter() {
-                match event {
-                    // Quit
-                    Event::Quit { .. }
-                    | Event::KeyDown {
-                        keycode: Some(Keycode::Escape),
-                        ..
-                    } => break 'main,
-                    Event::KeyDown {
-                        scancode: Some(scancode),
-                        ..
-                    } => {
-                        if let Some((port, bit)) = self.keymap(scancode) {
-                            self.cpu.set_bus_in_bit(port, bit, true);
-                        }
-                    }
-                    Event::KeyUp {
-                        scancode: Some(scancode),
-                        ..
-                    } => {
-                        if let Some((port, bit)) = self.keymap(scancode) {
-                            self.cpu.set_bus_in_bit(port, bit, false);
-                        }
-                    }
-                    _ => {}
-                }
-            }
+            self.handle_input(&mut events);
 
             // Run correct number of cycles, generate interrupts etc
-            let mut cycles: u32 = 0;
-
-            let mut halfway = false;
-            while cycles < cycles_per_frame {
-                cycles += self.cpu.step();
-                // Interrupts should happen in the middle of frame and at the end
-                if !halfway && (cycles > cycles_per_frame / 2) {
-                    cycles += self.cpu.interrupt(1);
-                    halfway = true;
-                }
-            }
-            self.cpu.interrupt(2);
+            self.run_cpu(cycles_per_frame);
 
             if self.cpu.get_display_update() {
                 canvas.set_draw_color(background_color);
@@ -213,24 +174,73 @@ impl Emu {
                 for y in 0..DISPLAY_HEIGHT {
                     for x in 0..DISPLAY_WIDTH {
                         if self.cpu.display(x, y) {
-                            canvas.draw_point(Point::new(x as i32, y as i32)).unwrap();
+                            canvas.draw_point(Point::new(x as i32, y as i32)).expect("Could not draw pixel on display");
                         }
                     }
                 }
 
                 // Copy grid texture on top to give a slight pixelated look
-                canvas.copy(&grid, None, None).unwrap();
+                canvas.copy(&grid, None, None).expect("Could not copy texture to canvas");
 
                 canvas.present();
 
                 self.cpu.set_display_update(false); // Cpu will set this to true whenever something changes on screen
             }
 
-            let sleep_duration =
-                (1_000_000_000_i64 / self.fps as i64) - t.elapsed().as_nanos() as i64;
+            self.sleep_before_next_frame(t);
+        }
+    }
 
-            if sleep_duration >= 0 {
-                sleep(Duration::new(0, sleep_duration as u32));
+    fn sleep_before_next_frame(&mut self, instant_at_start_of_frame: Instant) {
+        let sleep_duration =
+            (1_000_000_000_i64 / self.fps as i64) - instant_at_start_of_frame.elapsed().as_nanos() as i64;
+
+        if sleep_duration >= 0 {
+            sleep(Duration::new(0, sleep_duration as u32));
+        }
+    }
+
+    fn run_cpu(&mut self, cycles_per_frame: u32) {
+        let mut cycles: u32 = 0;
+
+        let mut halfway = false;
+        while cycles < cycles_per_frame {
+            cycles += self.cpu.step();
+            // Interrupts should happen in the middle of frame and at the end
+            if !halfway && (cycles > cycles_per_frame / 2) {
+                cycles += self.cpu.interrupt(1);
+                halfway = true;
+            }
+        }
+        self.cpu.interrupt(2);
+    }
+
+    fn handle_input(&mut self, events: &mut sdl2::EventPump) {
+        for event in events.poll_iter() {
+            match event {
+                // Quit
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => self.quit = true,
+                Event::KeyDown {
+                    scancode: Some(scancode),
+                    ..
+                } => {
+                    if let Some((port, bit)) = self.keymap(scancode) {
+                        self.cpu.set_bus_in_bit(port, bit, true);
+                    }
+                }
+                Event::KeyUp {
+                    scancode: Some(scancode),
+                    ..
+                } => {
+                    if let Some((port, bit)) = self.keymap(scancode) {
+                        self.cpu.set_bus_in_bit(port, bit, false);
+                    }
+                }
+                _ => {}
             }
         }
     }
